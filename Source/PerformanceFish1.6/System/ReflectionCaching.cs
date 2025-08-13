fea6b57e9e16ef960e9ef934795eb8393faeeb17
vjikholg <yuvict1@outlook.com>
2025-08-13 14:56:16 -0700

    test fix for reflections caching in RW1.6, swapping MonoField -> RTFieldInfo. If this doesnt work, most likely need to resolve FieldInfo dynamically

--- Source/PerformanceFish1.6/System/ReflectionCaching.cs
+++ Source/PerformanceFish1.6/System/ReflectionCaching.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2023 bradson
+// Copyright (c) 2023 bradson
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -752,7 +752,7 @@ public sealed class ReflectionCaching : ClassWithFishPatches
 			//}
 
 			public override MethodBase TargetMethodInfo { get; }
-				= AccessTools.DeclaredMethod(typeof(MonoField), nameof(MonoField.GetValue))!;
+				= AccessTools.DeclaredMethod(typeof(RuntimeFieldInfo), nameof(RuntimeFieldInfo.GetValue))!;
 
 			[MethodImpl(MethodImplOptions.AggressiveInlining)]
 			public static bool Prefix(FieldInfo __instance, object? obj, ref object? __result)
