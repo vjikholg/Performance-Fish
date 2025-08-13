375eeef8a964513951b697ce2a2b3c5d0687a88a
vjikholg <yuvict1@outlook.com>
2025-08-13 15:01:39 -0700

    added check for util/DebugAction.cs to use LudeonTK

--- Source/PerformanceFish1.6/Utility/DebugActions.cs
+++ Source/PerformanceFish1.6/Utility/DebugActions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2023 bradson
+// Copyright (c) 2023 bradson
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -6,6 +6,8 @@
 using System.Linq;
 using JetBrains.Annotations;
 #if V1_5
+using LudeonTK;
+#elif V1_6
 using LudeonTK;
 #endif
 using UnityEngine.Rendering;
